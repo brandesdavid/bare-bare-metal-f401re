2291885a01b06116a47c854ad8f641b6d4e0d58c
David Brandes <61701882+brandesdavid@users.noreply.github.com>
2024-05-02 15:56:51 +0200

    nice button addition

--- src/main.c
+++ src/main.c
@@ -7,8 +7,9 @@
 #include <stdint.h>
 #include "peripherals.h"
 
-#define MODER 2
-#define pin5 5
+#define MODER_BITS 2
+#define PIN_PA5 5
+#define PIN_PC13 13
 
 /**
  * @brief Struct Pointer for RCC Peripherals assigned with fixed address specified in reference manual
@@ -18,6 +19,12 @@ RCC_t   * const RCC     = (RCC_t    *)  0x40023800;
  * @brief Struct Pointer for GPIOA Peripherals assigned with fixed address specified in reference manual
  **/
 GPIOx_t * const GPIOA   = (GPIOx_t  *)  0x40020000;
+
+/**
+ * @brief Struct Pointer for GPIOC Peripherals assigned with fixed address specified in reference manual
+ **/
+GPIOx_t * const GPIOC   = (GPIOx_t  *)  0x40020800;
+
 
 /**
  *@brief This is a simple delay function implementation, that waits for <time>ms.
@@ -40,17 +47,31 @@ void wait_ms(int time){
 int main(void){
 
     //Enable Clock to GPIOA Peripheral
-    RCC->RCC_AHB1ENR |= 1;
+    RCC->RCC_AHB1ENR |= 0b001;
+
+    // enable clock to GPIOC
+    RCC->RCC_AHB1ENR |= 0b100;
     
     // Reset MODER Bitfield of PA5
-    GPIOA->GPIOx_MODER &= ~(3 << (pin5 * MODER));
-    // Write Value 1 to MODER Bitfield PA5 to configure PA5 as OUTPUT
-    GPIOA->GPIOx_MODER |=  (1 << (pin5 * MODER));
+    GPIOA->GPIOx_MODER &= ~(3 << (PIN_PA5 * MODER_BITS));
+    // Write Value 1 to MODER Bitfield PA5 to configure PA5 as "General purpose output mode"
+    GPIOA->GPIOx_MODER |=  (1 << (PIN_PA5 * MODER_BITS));
     
+    // Reset MODER Bitfield of PC13, which is also setting it as input mode (see reference manual 8.4.1)
+    GPIOC->GPIOx_MODER &= ~(3 << (PIN_PC13 * MODER_BITS));
+
     for(;;){
-        // Toggle PA5
-        GPIOA->GPIOx_ODR ^= (1 << pin5);
-        // Wait for 100ms
-        wait_ms(100);
+        /* der IDR13 ist fuer den input in PIN_PC13, wichtig: der button ist ein pull up resistor.
+            wenn der button down ist, dann ist IDR13 auf 0, wenn der button released ist, dann ist es auf 1*/
+        if (GPIOC->GPIOx_IDR & (1 << 13)) {
+            // Button is released (because the button is active low)
+           GPIOA->GPIOx_ODR &= ~(1 << PIN_PA5);
+        } else {
+            // Button is pressed
+            // Toggle PA5
+            GPIOA->GPIOx_ODR |= (1 << PIN_PA5);
+        }
+        
+        
     }
 }
